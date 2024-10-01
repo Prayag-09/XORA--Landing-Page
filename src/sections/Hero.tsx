59cc534c31ce8f696c7a1ffc870f57d537a8ba62
Prayag <prayagtushar2016@gmail.com>
2024-10-02 00:44:30 +0530

    removed border

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Button from '../components/Button';
 
 const Hero = () => {
 	return (
-		<section className=' relative border-2 pt-60 b-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32'>
+		<section className=' relative pt-60 b-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32'>
 			<Element name='hero'>
 				<div className=' container '>
 					<div className='realtive z-2 max-w-512 max-lg:max-w-388 '>
